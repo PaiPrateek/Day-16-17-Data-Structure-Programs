dec72f9d382d66e8d977801d0371073fe79cf841
Prateek Pai <paiprateek123@gmail.com>
2022-07-01 08:23:15 +0530

    Unordered List

--- DataStructurePrograms/Program.cs
+++ DataStructurePrograms/Program.cs
@@ -6,7 +6,271 @@ namespace DataStructurePrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Data Structure Programs");
+            Program p = new Program();
+            Console.WriteLine("Unordered List");
+
+            //Created student linked list as predefined list instaed of text from the file
+            p.addFirst("Prateek");
+            p.addFirst("Suhas");
+            p.addFirst("Prajwal");
+            p.addFirst("Sanath");
+            p.addFirst("Manjesh");
+            p.addFirst("Rakesh");
+            p.addFirst("Rohan");
+
+            Console.WriteLine("Enter Student Name:");
+            string input = Console.ReadLine();
+
+            bool check = p.search(input);
+
+            if (check==true)
+            {
+                // if input sata is present in the list then then that data is removed from the list
+                p.remove(input);
+            }
+            else
+            {
+                //if input data is not present in the list then that data is added into the list
+                p.addFirst(input);
+            }
+
+            //Display the data present in the list after the completing the above said operations.
+            p.diplayData();
+
+        }
+        // Node Class Creation
+        public class Node
+        {
+            public string data;
+            public Node next;
+            public Node(string data)
+            {
+                this.data = data;
+            }
+        }
+        //Head Creation
+        private Node head;
+
+        //Createing  Method to add element at last position 
+        public bool append(string data)
+        {
+            Node n = new Node(data);
+            if (head == null)
+            {
+                head = n;
+                return true;
+            }
+
+            Node t = head;
+            while (t.next != null)
+            {
+                t = t.next;
+            }
+            t.next = n;
+            return true;
+        }
+        //Creating method for add ing element at first postion - add()
+        public bool addFirst(string data)
+        {
+            Node n = new Node(data);
+            if (head == null)
+            {
+                head = n;
+                return true;
+            }
+            n.next = head;
+            head = n;
+            return true;
+        }
+        // Creating Method to Display the data in lIst
+        public void diplayData()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Linkedlist is Empty");
+            }
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        // Create method for Removing the latest entered item - pop()
+        public string pop()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is Empty");
+            }
+            Node t = head, p = head;
+            while (t.next != null)
+            {
+                p = t;
+                t = t.next;
+            }
+            string obj = t.data;
+            p.next = null;
+            return obj;
+        }
+
+        // CReating method for getting latest added item - peek()
+        public string peek()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is Empty");
+            }
+
+            Node t = head, p = head;
+            while (t.next != null)
+            {
+                p = t;
+                t = t.next;
+            }
+
+            string obj = t.data;
+            return obj;
+        }
+
+        // Creating method for searching - search()
+        public bool search(string data)
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is Empty");
+            }
+            Node t = head;
+            while (t != null)
+            {
+                if (t.data.Equals(data))
+                    return true;
+                t = t.next;
+            }
+            return false;
+        }
+        //Creating method for checking the list is empty or not - IsEmpty()
+        public bool IsEmpty()
+        {
+            if (head == null)
+            {
+                return true;
+            }
+                
+            return false;
+        }
+        //Craeting method for checking the size of the list - size()
+        public int size()
+        {
+            if (head == null)
+                return 0;
+            Node t = head; int count = 0;
+            while (t != null)
+            {
+                count++;
+                t = t.next;
+            }
+            return count;
+        }
+
+        public override string ToString()
+        {
+            if (head == null)
+            {
+
+                return null;
+            }
+            Node t = head;
+            string s = "";
+            while (t != null)
+            {
+                s = s + t.data + " ";
+                t = t.next;
+            }
+            return s;
+        }
+        //Creating method for getting the index of specified data
+        public int index(string data)
+        {
+            int count = -1;
+            if (head == null)
+                return -1;
+            Node t = head;
+            while (t != null)
+            {
+                count++;
+                if (t.data.Equals(data))
+                    return count;
+                t = t.next;
+            }
+            return -1;
+        }
+        //Creating method for inserting the data inthe specified index position
+        public bool insert(int ind, string data)
+        {
+            Node n = new Node(data);
+            if (ind == 0)
+            {
+                n.next = head.next;
+                head = n;
+                return true;
+            }
+            Node t = head, pre = null;
+            while (ind > 0 && t != null)
+            {
+                ind--;
+                pre = t;
+                t = t.next;
+            }
+            if (ind == 0)
+            {
+                pre.next = n;
+                n.next = t;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Index is not in the specified Range");
+                return false;
+            }
+        }
+        // Creating method to Delete the elment from the Linked List 
+        //public void delete(int index)
+        //{
+        //    if (index == 0)
+        //    {
+        //        head = head.next;
+        //        return;
+        //    }
+        //    Node Temp = head;
+        //    for (int i = 0; i < index - 1; i++)
+        //    {
+        //        Temp = Temp.next;
+        //    }
+        //    Temp.next = Temp.next.next;
+        //}
+
+        // Creating method to Delete the elment from the Linked List 
+        public bool remove(string data)
+        {
+            if (head == null)
+            {
+                Console.WriteLine("List is Empty");
+            }
+            if (head.next == null)
+                head = null;
+            Node t = head, pre = null;
+            while (t != null)
+            {
+                if (t.data.Equals(data))
+                {
+                    pre.next = t.next;
+                    return true;
+                }
+                pre = t;
+                t = t.next;
+            }
+            return false;
         }
     }
 }
